802a2acd736d35bb066419b1d56cbd795f7766b9
RadekSedlacek1 <sedlacek.radek@seznam.cz>
2025-03-20 14:09:52 +0100

    Ban user function

--- blog_website/main/views.py
+++ blog_website/main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm, PostForm
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import User, Group
 from .models import Post
 
 # Create your views here.
@@ -12,15 +13,36 @@ def index(request):
 
     if request.method == "POST":
         post_id = request.POST.get("post-id")
-        post = Post.objects.filter(id=post_id).first()
-        if post and post.author == request.user:
-            post.delete()
-        print(post_id)
+        user_id = request.POST.get("user-id")
+        
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
+                post.delete()
+            print(f"{post} deleted")
 
+        elif user_id:
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name="default")
+                    group.user_set.remove(user)
+                    print(f"{user} removed from default")
+                except:
+                    pass
+                
+                try:
+                    group = Group.objects.get(name="mod")
+                    group.user_set.remove(user)
+                    print(f"{user} removed from mod")
+                except:
+                    pass
+                
     return render(request, 'main/index.html', {"posts":posts})
 
 
 @login_required(login_url="/login")
+@permission_required("main.add_post", login_url="/login", raise_exception=True)
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
